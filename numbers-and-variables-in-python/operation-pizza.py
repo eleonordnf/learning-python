a75d1d8eeb48a4f4992378afe0c79eaf0c8bc691
Eleonor Diaz Nauton-Fourteu <eleonordnf@gmail.com>
2024-01-14 17:26:46 +0100

    ch 03 v3

--- numbers-and-variables-in-python/operation-pizza.py
+++ numbers-and-variables-in-python/operation-pizza.py
@@ -1,15 +1,15 @@
-# look at chapter tree Atlanta pizza
+# Ask the person how many pizzas they want, get the # with eval()
 number_of_pizzas = eval(input("how many pizzas do you want?"))
-
+# Ask for the menu cost of each pizza
 cost_per_pizzas = eval(input("how much does each pizza cost?"))
-
+# Calculate the total cost of the pizzas as our subtotal
 subtotal = number_of_pizzas * cost_per_pizzas
-
-tax_rate = 0.08
+# Calculate the sales tax owed, at 8% of the subtotal
+tax_rate = 0.08 # we store 8% as the decimal value 0.08
 sales_tax = subtotal * tax_rate
-
+# Add the sales tax to the subtotal for the final total
 total = subtotal + sales_tax
-
+# Show the user the total amount due, including tax
 print("the total cost is $", total)
 print("this includes $", subtotal, "for pizza and")
 print("$", sales_tax, "in sales tax.")
