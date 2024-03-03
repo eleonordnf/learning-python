111a865116c96f3bd857acf6a2acb232384e77cd
Eleonor Diaz Nauton-Fourteu <eleonordnf@gmail.com>
2024-03-03 12:32:15 +0100

    ch 05 v1

---
+++ conditions/MyOwnConditions.py
@@ -0,0 +1,5 @@
+Your_Age = eval(input("how old are you?"))
+if Your_Age >= 10:
+    print("your old enough to do gymnastics!")
+if Your_Age < 10:
+    print("sorry you can not do gymnastics until your 10")
---
+++ conditions/OldEnoughToDrive.py
@@ -0,0 +1,9 @@
+# OldEnoughOrElse.py
+driving_age = eval(input("What is the legal driving age where you live? "))
+your_age = eval(input("How old are you? "))
+if your_age >= driving_age:
+    print("You're old enough to drive!")
+else:
+    print("Sorry, you can drive in", driving_age - your_age, "years.")
+# "else": but you can also put "if your_age < driving_age:"
+
